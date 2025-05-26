20b836c8f0fedf03940785a0763e942972d35260
choeyunseo <choeyunseo4060@naver.com>
2025-05-27 01:32:26 +0900

    Fix: boj 14500

--- backTracking/BJ14500.java
+++ backTracking/BJ14500.java
@@ -33,79 +33,80 @@ public class BJ14500 {
 
         // 방문배열 초기화
         visited = new boolean[n][m];
-        
-
-
+    
         // 함수 호출
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                // visited 초기화
-                general(i, j,  1, graph[i][j], graph, visited);
+                visited[i][j] = true;
+                general(i, j,  1, graph[i][j]);
                 visited[i][j] = false;
             }
         }
 
-        // T자 함수 호출
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int tSum = tShape(i, j, 0, 0);
-                maxTSum = Math.max(maxTSum, tSum);
-            }
-        }
+        // // T자 함수 호출
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < m; j++) {
+        //         int tSum = tShape(i, j, 0, 0);
+        //         maxTSum = Math.max(maxTSum, tSum);
+        //     }
+        // }
 
         System.out.println(Math.max(maxSum, maxTSum));
+        scanner.close();
     }
 
-    public static void general(int x, int y, int depth, int sum, int[][] graph, boolean[][] visited) {
-        visited[x][y] = true;
-        // sum += graph[x][y];
-        // 4개 모두 채웠을 경우
+    public static void general(int x, int y, int depth, int sum) {
         if (depth == 4) {
             maxSum = Math.max(maxSum, sum);
             return;
         }
-
         // 동서남북 보면서
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             // 범위 && 방문하지 않았으면
             if ((0 <= nx && nx < n) && (0 <= ny && ny < m) && visited[nx][ny] == false) {
-                // 테트로미노의 값 더하기
+                // T shape
+                if (depth == 2) {
+                    visited[nx][ny] = true;
+                    general(x, y, depth + 1, sum + graph[nx][ny]);
+                    visited[nx][ny] = false;
+                    // return; // -> return 하면 안 됨. 깊이가 2인 다른 경우도 봐야하므로
+                }
+                // T shape을 제외한 나머지 테트로미노
                 visited[nx][ny] = true;
-                general(nx, ny, depth + 1, sum += graph[nx][ny], graph, visited);
-                sum -= graph[nx][ny];
+                general(nx, ny, depth + 1, sum + graph[nx][ny]);
                 visited[nx][ny] = false;
             }
         }
     }
 
-    // ㅗ, ㅜ, ㅏ, ㅓ (T자)
-    public static int tShape(int x, int y, int depth, int max) {
-        // 동서남북 말고 T자로 보기
-        for (int[][] shape : tShapes) { // int[][] shape = {{0, 0}, {1, 0}, {2, 0}, {1, 1}} 
-            boolean isOk = true;
-            int sum = 0; // 각 모양마다 sum 초기화
-            for (int[] d : shape) { // int[] d = {0, 0} 
-                int nx = x + d[0];
-                int ny = y + d[1];
+    // // ㅗ, ㅜ, ㅏ, ㅓ (T자)
+    // public static int tShape(int x, int y, int depth, int max) {
+    //     // 동서남북 말고 T자로 보기
+    //     for (int[][] shape : tShapes) { // int[][] shape = {{0, 0}, {1, 0}, {2, 0}, {1, 1}} 
+    //         boolean isOk = true;
+    //         int sum = 0; // 각 모양마다 sum 초기화
+    //         for (int[] d : shape) { // int[] d = {0, 0} 
+    //             int nx = x + d[0];
+    //             int ny = y + d[1];
 
-                // 범위 체크
-                if ((0 <= nx && nx < n) && (0 <= ny && ny < m)) {
-                    // 모든 좌표들이 범위 안에 있다면 sum 구하기
-                    sum += graph[nx][ny];
-                }
-                else {
-                    isOk = false;
-                    break;
-                }
-            }
-            if (isOk) {
-                // 최댓값 갱신 
-                max = Math.max(sum, max);
-            }
+    //             // 범위 체크
+    //             if ((0 <= nx && nx < n) && (0 <= ny && ny < m)) {
+    //                 // 모든 좌표들이 범위 안에 있다면 sum 구하기
+    //                 sum += graph[nx][ny];
+    //             }
+    //             else {
+    //                 isOk = false;
+    //                 break;
+    //             }
+    //         }
+    //         if (isOk) {
+    //             // 최댓값 갱신 
+    //             max = Math.max(sum, max);
+    //         }
             
-        }
-        return max;
-    }
+    //     }
+    //     return max;
+    // }
 }
