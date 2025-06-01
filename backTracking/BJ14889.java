7d31af826d03dcbdfb518a1d34ae67cf8420834a
choeyunseo <choeyunseo4060@naver.com>
2025-06-01 19:58:08 +0900

    Fix: boj 14889

--- backTracking/BJ14889.java
+++ backTracking/BJ14889.java
@@ -3,10 +3,9 @@ import java.util.*;
 
 public class BJ14889 {
     static int n;
-    static int minSkill = Integer.MAX_VALUE; // 최댓값으로 설정.
+    static int minGap = Integer.MAX_VALUE;
     static int[][] graph;
-    static ArrayList<Integer> teamA = new ArrayList<>();
-    static ArrayList<Integer> teamIndex = new ArrayList<>();
+    static ArrayList<Integer> teamArr = new ArrayList<>();
     
     public static void main(String[] args) {
         // 입력
@@ -20,75 +19,48 @@ public class BJ14889 {
         }
 
         makeTeam(0);
-        System.out.println(minSkill);
+        System.out.println(minGap);
     }
 
     public static void makeTeam(int index) {
-        
         // 팀원이 완성되면
-        if (teamA.size() == n / 2) {
-            // 나머지 팀 구성
-            ArrayList<Integer> teamB = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                if (!teamA.contains(i)) {
-                    teamB.add(i);
+        if (teamArr.size() == n / 2) {
+            // 전체 팀구성 (어차피 2팀밖에 없으므로 T/F 사용하였음.)
+            // ex. team = [t, t, t, f, f, f]
+            boolean[] team = new boolean[n]; // 기본값으로 모두 false 
+            for (int i : teamArr) {
+                team[i] = true; // start팀
+            }
+
+            // 능력치 구하기 
+            // 2명씩 뽑아서 능력치를 구할 때, 함수 사용하지 않고 이중 for문으로 2명씩 뽑는 모든 조합을 구할 수 있음.
+            int startSkill = 0;
+            int linkSkill = 0;
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++) { // i + 1부터 (중복x)
+                    // start팀
+                    if (team[i] && team[j]) {
+                        startSkill += graph[i][j];
+                        startSkill += graph[j][i];
+                    }
+                    // link팀
+                    else if (!team[i] && !team[j]) {
+                        linkSkill += graph[i][j];
+                        linkSkill += graph[j][i];
+                    }
                 }
             }
-
-            /* ------------- 인덱스 조합하기 ------------ */
-            // 팀 A
-            ArrayList<int[]> returnIndex = new ArrayList<>();
-            makeTeamIndex(teamA, 0, returnIndex);
-    
-            // 팀 B
-            ArrayList<int[]> returnIndexB = new ArrayList<>();
-            makeTeamIndex(teamB, 0, returnIndexB);
-
-            /* ------------- 능력치 구하기 ------------ */
-            // team A
-            int teamASkill = 0;
-            for (int i = 0; i < returnIndex.size(); i++) {
-                int skill = 0; // 초기화
-                teamASkill += graph[returnIndex.get(i)[0]][returnIndex.get(i)[1]];
-                teamASkill += graph[returnIndex.get(i)[1]][returnIndex.get(i)[0]];
-                teamASkill += skill;
-            }
-
-            // Team B
-            int teamBSkill = 0;
-            for (int i = 0; i < returnIndexB.size(); i++) {
-                int skill = 0;
-                teamBSkill += graph[returnIndexB.get(i)[0]][returnIndexB.get(i)[1]];
-                teamBSkill += graph[returnIndexB.get(i)[1]][returnIndexB.get(i)[0]];
-                teamBSkill += skill;
-            }
-
-            // 능력치 차이 구하기
-            int skillGap = Math.abs(teamASkill - teamBSkill);
-            minSkill = Math.min(minSkill, skillGap);
+            // 최솟값 갱신
+            minGap = Math.min(Math.abs(startSkill - linkSkill), minGap);
+            return;
         }
 
         for (int i = index; i < n; i++) {
-            // 만약 teamA에 없다면
-            if (!teamA.contains(i)) {
-                teamA.add(i);
+            if (!teamArr.contains(i)) {
+                teamArr.add(i);
                 makeTeam(i + 1);
-                teamA.remove(teamA.size() - 1);
+                teamArr.remove(teamArr.size() - 1);
             }
-        }
-    }
-     
-    // 팀원 조합
-    public static void makeTeamIndex(ArrayList<Integer> arr, int index, ArrayList<int[]> returnIndex) {
-        if (teamIndex.size() == 2) {
-            int[] addValue = {teamIndex.get(0), teamIndex.get(1)}; 
-            returnIndex.add(addValue); // 깊은 복사로 넣기.
-            return;
-        }
-        for (int i = index; i < arr.size(); i++) {
-                teamIndex.add(arr.get(i));
-                makeTeamIndex(arr, i + 1, returnIndex);
-                teamIndex.remove(teamIndex.size() - 1);
         }
     }
 }
