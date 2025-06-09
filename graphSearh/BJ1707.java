7a979458990ec29bb8a25cdab5c2b0efbd395748
choeyunseo <choeyunseo4060@naver.com>
2025-06-09 16:14:58 +0900

    Fix: boj 1707

--- graphSearh/BJ1707.java
+++ graphSearh/BJ1707.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class BJ1707 {
     static int k;
-    static int[][] graph;
+    static ArrayList<ArrayList<Integer>> adjList; // 인접행렬 -> 인접리스트 (메모리 절약)
     static int[] visited; // int로 한 이유는, 방문하지 않았으면 무조건 0. color를 구분하기 위하여 1과 -1를 사용하기 위함.
 
     public static void main(String[] args) {
@@ -14,25 +14,32 @@ public class BJ1707 {
         for (int i = 0; i < k; i++) {
             int v = scanner.nextInt();
             int e = scanner.nextInt();
-            graph = new int[v][v]; // graph 초기화
+            adjList = new ArrayList<>(); // 인접리스트 초기화
             visited = new int[v]; // visited 초기화
+
+            for (int j = 0; j < v; j++) {
+                adjList.add(new ArrayList<>());
+            }
 
             // 간선 수 만큼 간선 입력받기
             for (int j = 0; j < e; j++) {
-                int edge1 = scanner.nextInt();
-                int edge2 = scanner.nextInt();
-                graph[edge1 - 1][edge2 - 1] = 1; // 양방향 처리 
-                graph[edge2 - 1][edge1 - 1] = 1;
+                int a = scanner.nextInt() - 1;
+                int b = scanner.nextInt() - 1;
+                adjList.get(a).add(b); // 양방향 연결
+                adjList.get(b).add(a);
             }
 
-            boolean result = bfs(0 ,v); // test case만큼 bfs함수 호출.
-            if (result) {
-                System.out.println("YES");
+            // 비연결 그래프일 수도 있으니, 방문하지 않은 모든 정점에서 함수 실행해야 함.
+            boolean isFalse = true;
+            for (int j = 0; j < v; j++) {
+                if (visited[j] == 0) {
+                    if (!bfs(j, v)) {
+                        isFalse = false;
+                        break;
+                    }
+                }
             }
-            else {
-                System.out.println("NO");
-            }
-
+            System.out.println(isFalse ? "YES" : "NO");
         }
     }
 
@@ -46,21 +53,17 @@ public class BJ1707 {
             int curVertex = queue.poll(); // 정점 번호.
             
             // curVertex와 이웃한 노드 찾기
-            for (int i = 0; i < n; i++) {
-                // 이웃했다면 
-                if (graph[curVertex][i] == 1) {
-                    int nextVertex = i; // 어차피 양방향 간선이므로 한 방향만 봐도 됨.
-                    // 방문하지 않았다면
-                    if (visited[nextVertex] == 0) {
-                        visited[nextVertex] = -visited[curVertex];
-                        queue.add(nextVertex);
-                    }
-                    // 방문했다면
-                    else {
-                        // 현재와 같은 색상이면 false
-                        if (visited[curVertex] == visited[nextVertex]) {
-                            return false;
-                        }
+            for (int nextVertex : adjList.get(curVertex)) {
+                // 방문하지 않았다면
+                if (visited[nextVertex] == 0) {
+                    visited[nextVertex] = -visited[curVertex];
+                    queue.add(nextVertex);
+                }
+                // 방문했다면
+                else {
+                    // 현재와 같은 색상이면 false
+                    if (visited[curVertex] == visited[nextVertex]) {
+                        return false;
                     }
                 }
             }
