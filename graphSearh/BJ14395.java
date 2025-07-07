a56b2e116260b09d6f734532f930581e43aeb8ed
choeyunseo <choeyunseo4060@naver.com>
2025-07-07 19:26:49 +0900

    Solve: boj 14395

--- graphSearh/BJ14395.java
+++ graphSearh/BJ14395.java
@@ -4,14 +4,13 @@ import java.io.*;
 import java.util.*;
 
 public class BJ14395 {
-    static int s, t;
-    // static char[] operator = {'*', '+', '-', '/'};
-    static Set<Integer> visited = new HashSet<>();
+    static long s, t;
+    static Set<Long> visited = new HashSet<>();
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] line = br.readLine().split(" ");
-        s = Integer.parseInt(line[0]);
-        t = Integer.parseInt(line[1]);
+        s = Long.parseLong(line[0]);
+        t = Long.parseLong(line[1]);
 
         // s와 t가 같다면
         if (s == t) {
@@ -27,22 +26,20 @@ public class BJ14395 {
         queue.add(new String[]{String.valueOf(s), ""}); // [0]: 숫자, [1]: 경로
         visited.add(s);
 
-
         while (!queue.isEmpty()) {
             String[] cur = queue.poll();
-            int num = Integer.parseInt(cur[0]);
+            long num = Long.parseLong(cur[0]);
             String path = cur[1];
 
             // t와 같다면
             if (num == t) {
-                // 경로 출력 
                 System.out.println(path);
-                System.exit(0); // 바로 종료 
+                System.exit(0);
             }
 
             // 모든 연산
             for (int i = 0; i < 4; i++) {
-                int newNum = 0; 
+                long newNum = 0; 
                 String op= ""; 
                 if (i == 0) {
                     newNum = (num * num);
@@ -62,8 +59,8 @@ public class BJ14395 {
                     op = "/";
                 }
 
-                // 범위 && 방문하지 않음 
-                if (0 <= newNum && newNum <= t && visited.add(newNum)) { 
+                // 방문하지 않았으면
+                if (visited.add(newNum)) { 
                     queue.add(new String[]{String.valueOf(newNum), path + op});
                 }
             }
