ab5902edacff09d2571dab206efb1320b3fe1891
choeyunseo <choeyunseo4060@naver.com>
2025-07-28 17:05:35 +0900

    Solve: boj 9370

--- graphSearh/BJ9370.java
+++ graphSearh/BJ9370.java
@@ -4,10 +4,10 @@ import java.util.*;
 import java.io.*;
 
 public class BJ9370 {
-    static int tc, n, m, t, s, g, h, a, b, c;
-    static ArrayList<Node>[] list; // 인접 리스트
+    static int tc, n, m, t, s, g, h, a, b, c, INF = Integer.MAX_VALUE;
+    static ArrayList<Node>[] list;
     static int[] dist;
-    static int[] candidates; // 목적지 후보
+    static int[] candidates;
     static boolean[] visited;
 
     public static class Node {
@@ -33,7 +33,7 @@ public class BJ9370 {
 
             String[] line2 = br.readLine().split(" ");
             s = Integer.parseInt(line2[0]) - 1; // 출발지
-            g = Integer.parseInt(line2[1]) - 1; 
+            g = Integer.parseInt(line2[1]) - 1;
             h = Integer.parseInt(line2[2]) - 1;
 
             list = new ArrayList[n];
@@ -54,7 +54,7 @@ public class BJ9370 {
                 c = Integer.parseInt(edges[2]);
                 list[a].add(new Node(b, c));
                 list[b].add(new Node(a, c));
-            }   
+            }
 
             // 목적지 후보 입력
             for (int j = 0; j < t; j++) {
@@ -62,62 +62,54 @@ public class BJ9370 {
                 candidates[j] = x - 1;
             }
 
-            // 다익스트라 호출
+            // 한 번에 가는 값과 거쳐서 가는 값이 같은지 비교하기
             for (int j = 0; j < t; j++) {
                 int path1 = 0;
                 int path2 = 0;
-                // 1. 최단 거리
+                int path3 = 0;
+
                 path1 += dijkstra(s, candidates[j]);
 
-                Arrays.fill(dist, Integer.MAX_VALUE);
-                Arrays.fill(visited, false);
-
-                // 2. g, h를 거쳤을 때
                 path2 += dijkstra(s, g);
-                Arrays.fill(dist, Integer.MAX_VALUE);
-                Arrays.fill(visited, false);
-
                 path2 += dijkstra(g, h);
-
-                Arrays.fill(dist, Integer.MAX_VALUE);
-                Arrays.fill(visited, false);
-
                 path2 += dijkstra(h, candidates[j]);
 
-                Arrays.fill(dist, Integer.MAX_VALUE);
-                Arrays.fill(visited, false);
+                path3 += dijkstra(s, h);
+                path3 += dijkstra(h, g);
+                path3 += dijkstra(g, candidates[j]);
 
-                System.out.println("path1: " + path1 + ", path2: " + path2);
-
-                // path1과 path2가 같으면
-                if (path1 == path2) {
+                if (path1 == Math.min(path2, path3)) {
                     printCandidates.add(candidates[j]);
                 }
             }
 
-            Collections.sort(printCandidates); // 오름차순 정렬
+            // 오름차순 정렬 후 출력
+            Collections.sort(printCandidates);
             for (int j = 0; j < printCandidates.size(); j++) {
                 System.out.print(printCandidates.get(j) + 1 + " ");
             }
-            System.out.println();    
+            System.out.println();
         }
     }
 
     public static int dijkstra(int start, int x) {
+        int[] dist = new int[n];
+        boolean[] visited = new boolean[n];
+        Arrays.fill(dist, INF);
+
         PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
         pq.offer(new Node(start, 0));
         dist[start] = 0;
 
         while (!pq.isEmpty()) {
             Node cur = pq.poll();
-            
 
             if (visited[cur.vertex]) {
                 continue;
             }
             visited[cur.vertex] = true;
 
-            for (Node next: list[cur.vertex]) {
+            for (Node next : list[cur.vertex]) {
                 if (dist[next.vertex] > dist[cur.vertex] + next.weight) {
                     dist[next.vertex] = dist[cur.vertex] + next.weight;
                     pq.offer(new Node(next.vertex, dist[next.vertex]));
