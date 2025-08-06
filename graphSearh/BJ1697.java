35294a99417cc7e1e19081a6587fee7c1c9243a0
choeyunseo <choeyunseo4060@naver.com>
2025-08-06 13:56:08 +0900

    Fix: boj 1697

--- graphSearh/BJ1697.java
+++ graphSearh/BJ1697.java
@@ -3,58 +3,40 @@ package graphSearh;
 import java.util.*;
 
 public class BJ1697 {
-    static int n;
-    static int k;
-    static int[] second = new int[100001];
+    static int n, k;
+    static int[] ways = {1, -1, 2};
     static boolean[] visited = new boolean[100001];
-    
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        k = scanner.nextInt();
 
-        bfs(n); // 수빈이의 현재 위치부터 시작.
+    public static void main (String[] main) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        solve(n);
     }
 
-    public static void bfs(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        second[start] = 0; // 시작은 0초부터
+    public static void solve(int n) { // bfs
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {n, 0}); // 시간은 0초부터
+        visited[n] = true;
 
         while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            // 동생을 찾았다면
-            if (cur == k) {
-                System.out.println(second[cur]);
+            int cur[] = queue.poll();
+
+            // 수빈 == 동생
+            if (cur[0] == k) {
+                System.out.println(cur[1]);
                 break;
             }
-
-            // 가능한 모든 3가지 방법 다 보기
-            for (int i = 0; i < 3; i++) {
-                int next;
-                // x + 1
-                if (i == 0) {
-                    next = cur + 1;
+            
+            // 3가지의 위치 모두 보기
+            int newPos;
+            for (int i : ways) {
+                newPos = (i != 2) ? cur[0] + i : cur[0] * i;
+                if (0 <= newPos && newPos <= 100000 && !visited[newPos]) {
+                    queue.add(new int[] {newPos, cur[1] + 1});
+                    visited[newPos] = true;
                 }
-                // x - 1
-                else if (i == 1) {
-                    next = cur - 1;
-                }
-                // 2 * x
-                else {
-                    next = 2 * cur;
-                }
-
-                // 범위 (0보다 커야함!)
-                if (0 <= next && next <= 100000) { 
-                    // 방문하지 않은 위치만
-                    if (!visited[next]) {
-                        queue.add(next);
-                        visited[next] = true;
-                        second[next] = second[cur] + 1;
-                    }
-                }
-            }            
+            }
         }
     }
 }
