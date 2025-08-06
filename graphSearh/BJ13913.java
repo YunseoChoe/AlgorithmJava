e79dee7efe22727da9250a46baf0231010b30f7e
choeyunseo <choeyunseo4060@naver.com>
2025-08-06 15:22:30 +0900

    Fix: boj 13913

--- graphSearh/BJ13913.java
+++ graphSearh/BJ13913.java
@@ -3,73 +3,56 @@ package graphSearh;
 import java.util.*;
 
 public class BJ13913 {
-    static int n;
-    static int k;
-    static int[] path = new int[100001];
-    static int[] second = new int[100001];
-    static boolean[] visited = new boolean[100001];
-    
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        k = scanner.nextInt();
+    static int n, k, MAX = 100000;
+    static int[] prev = new int[MAX + 1];
+    static ArrayList<Integer> reversePath = new ArrayList<>();
+    static boolean[] visited = new boolean[MAX + 1];
 
-        bfs(n); // 수빈이의 현재 위치부터 시작.
+    public static void main (String[] main) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        solve(n);
     }
 
-    public static void bfs(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        second[start] = 0; // 시작은 0초부터
-        path[start] = -1; // 시작 위치.
-        visited[start] = true;
+    public static void solve(int n) { // bfs
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {n, 0}); // 시간은 0초부터.
+        prev[n] = -1; // 시작점의 이전 경로는 -1로 세팅.
+        visited[n] = true;
 
         while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            // 동생을 찾았다면
-            if (cur == k) {
-                System.out.println(second[cur]);
-                // path 출력 (경로를 거꾸로 저장 후 출력.)
-                ArrayList<Integer> reversePath = new ArrayList<>();
-                reversePath.add(cur); 
-                while (path[cur] != -1) {
-                    reversePath.add(path[cur]);    
-                    cur = path[cur];
-                }
+            int cur[] = queue.poll();
+
+            // 수빈 == 동생
+            if (cur[0] == k) {
+                System.out.println(cur[1]);
+
+                backtracking(prev);
                 Collections.reverse(reversePath); 
-                for (int i = 0; i < reversePath.size(); i++) {
-                    System.out.print(reversePath.get(i) + " ");
+                for (int i : reversePath) {
+                    System.out.print(i + " ");
                 }
                 break;
             }
+            
+            // 3가지의 위치 확인
+            int[] nextPositions = {cur[0] - 1, cur[0] + 1, cur[0] * 2};
+            for (int next : nextPositions) {
+                if (0 <= next && next <= 100000 && !visited[next]) {
+                    queue.add(new int[] {next, cur[1] + 1});
+                    visited[next] = true;
+                    prev[next] = cur[0]; // 경로 저장
+                }
+            }
+        }
+    }
 
-            // 가능한 모든 3가지 방법 다 보기
-            for (int i = 0; i < 3; i++) {
-                int next;
-                // x + 1
-                if (i == 0) {
-                    next = cur + 1;
-                }
-                // x - 1
-                else if (i == 1) {
-                    next = cur - 1;
-                }
-                // 2 * x
-                else {
-                    next = 2 * cur;
-                }
-
-                // 범위 (0보다 커야함)
-                if (0 <= next && next <= 100000) { 
-                    // 방문하지 않은 위치만
-                    if (!visited[next]) {
-                        queue.add(next);
-                        visited[next] = true;
-                        second[next] = second[cur] + 1;
-                        path[next] = cur;
-                    }
-                }
-            }            
+    public static void backtracking(int[] prev) {
+        int i = k;
+        while (i != -1) {
+            reversePath.add(i); 
+            i = prev[i]; 
         }
     }
 }
