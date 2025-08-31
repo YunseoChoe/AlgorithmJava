566a336fa515cba905c5049c817c7c4247296a17
choeyunseo <choeyunseo4060@naver.com>
2025-09-01 00:17:06 +0900

    Fail: boj 2156

--- DynamicProgramming/BJ2156.java
+++ DynamicProgramming/BJ2156.java
@@ -14,19 +14,39 @@ public class BJ2156 {
             arr[i] = sc.nextInt();
         }
 
-        solve();
+        System.out.println(solve());
     }
 
-    public static void solve() {
+    public static int solve() {
         int[] dp = new int[n];
+
+        if (n == 1) {
+            return arr[0];
+        }
+
+        else if (n == 2) {
+            return arr[0] + arr[1];
+        }
 
         // 초기값
         dp[0] = arr[0];
         dp[1] = arr[0] + arr[1];
-        dp[2] = Math.max(arr[0] + arr[2], arr[1] + arr[2]);
+        dp[2] = Math.max(arr[0] + arr[1], Math.max(arr[0] + arr[2], arr[1] + arr[2]));
 
+        // dp 점화식
         for (int i = 3; i < n; i++) {
-            dp[i] = 
+            dp[i] = Math.max(dp[i - 2], dp[i - 3] + arr[i - 1]) + arr[i];
         }
+
+        // for (int i = 0; i < n; i++) {
+        //     System.out.print(dp[i] + " ");
+        // }
+
+        int maxValue = 0;
+        for (int i = 0; i < n; i++) {
+            maxValue = (maxValue < dp[i]) ? dp[i] : maxValue;   
+        }
+
+        return maxValue;
     }
 }
