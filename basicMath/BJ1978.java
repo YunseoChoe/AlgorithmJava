e93fe9d6c2aa1c88fe9ccf61888e1392375b1c74
choeyunseo <choeyunseo4060@naver.com>
2025-10-31 14:42:29 +0900

    Fix: boj 1978

--- basicMath/BJ1978.java
+++ basicMath/BJ1978.java
@@ -6,34 +6,17 @@ import java.io.*;
 public class BJ1978 {
     static int n;
 
-    public static boolean isPrimeNumber(int num) {
-        ArrayList<Integer> divisor = new ArrayList<>();
-        if (num > 1) {
-            // 모든 약수 구하기 (divisor)
-            for (int i = 1; i <= num; i++) {
-                if (num % i == 0) {
-                    divisor.add(i);
-                }
-            }
-            // 약수가 1과 자기 자신뿐이면
-            if (divisor.size() == 2) {
-                if (divisor.get(0) == 1 && divisor.get(1) == num) {
-                    return true;
-                }
+    public static boolean solve(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i < num; i++) {
+            // 소수가 아닌 조건 (2보다 크고, num보다 작은 수 중 0으로 나눠 떨어지는 값이 있으면)
+            if (num % i == 0) {
+                return false;
             }
         }
-        return false;
-    }
-
-    public static int solve(int[] arr) {
-        // TODO: arr중 소수 개수 반환
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (isPrimeNumber(arr[i])) {
-                cnt++;
-            }
-        }
-        return cnt;
+        return true;
     }
 
     public static void main(String[] args) throws IOException {
@@ -47,6 +30,13 @@ public class BJ1978 {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(solve(arr));
+        int cnt = 0;
+        for (int x : arr) {
+            if (solve(x)) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
     }
 }
