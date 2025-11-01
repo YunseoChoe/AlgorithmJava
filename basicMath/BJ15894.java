516db57228a2df441fca8e201e5e427890d01eea
choeyunseo <choeyunseo4060@naver.com>
2025-11-01 13:43:49 +0900

    Solve: boj 15894

--- basicMath/BJ15894.java
+++ basicMath/BJ15894.java
@@ -4,35 +4,16 @@ import java.util.*;
 import java.io.*;
 
 public class BJ15894 {
-    static int n;
-    static int[] dp;
+    static long n; // n의 범위는 10^9이므로 int가 아닌 long으로 해야 맞음.
 
-    public static int solve() {
-        if (n == 1) {
-            return 4;
-        }
-        else if (n == 2) {
-            return 6;
-        }
-        else if (n == 3) {
-            return 12;
-        }
-
-        // dp 초기값
-        dp[3] = 12;
-
-        // dp 점화식
-        for (int i = 4; i <= n; i++) {
-            dp[i] = dp[i - 1] - (n - 1) + (3 + n);
-        }
-
-        return dp[n];
+    public static long solve() {
+        return n * 4;
     }
+    
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        dp = new int[n + 1];
 
-        System.out.println(solve());
+        System.out.println(solve());   
     }
 }
