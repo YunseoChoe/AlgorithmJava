a49eba5edd80be73b0ff7cf56fa95dae719cf223
choeyunseo <choeyunseo4060@naver.com>
2025-11-11 07:52:22 +0900

    Solve: psg n개의 최소공배수

---
+++ 프로그래머스/PSGN개의최소공배수.java
@@ -0,0 +1,29 @@
+package 프로그래머스;
+
+public class PSGN개의최소공배수 {
+    // 최대공약수
+    public int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+    
+    // 최소공배수
+    public int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+    
+    public int solution(int[] arr) {
+        int answer = 0;
+        int lcmValue = arr[0];
+        
+        // 2개씩 다 보기
+        for (int i = 1; i < arr.length; i++) {
+            lcmValue = lcm(lcmValue, arr[i]);
+        }
+        
+        answer = lcmValue;
+        return answer;
+    }
+}
