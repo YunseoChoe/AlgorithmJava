2501e282cf36a379ca8b75dff2eba57910705709
choeyunseo <choeyunseo4060@naver.com>
2025-11-14 14:28:05 +0900

    Solve: psg 방금그곡

--- 프로그래머스/PSG방금그곡.java
+++ 프로그래머스/PSG방금그곡.java
@@ -1,49 +1,52 @@
 package 프로그래머스;
 
 public class PSG방금그곡 {
+    public String convertSharp(String s) {
+        return s.replaceAll("C#", "c")
+                .replaceAll("D#", "d")
+                .replaceAll("F#", "f")
+                .replaceAll("G#", "g")
+                .replaceAll("A#", "a");
+    }
+    
     public int convertToMin(String time) {
-        // 총 분 변환 함수
-        // ex) 12:00 -> 720(m)
-        
         String[] t = time.split(":");
-        int hour = Integer.parseInt(t[0]);
-        int min = Integer.parseInt(t[1]);
-        
-        return hour * 60 + min;
+        return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
     }
     
     public String buildMelody(String melody, int playTime) { 
-        // 최종 재생 멜로디 만들기
         StringBuilder sb = new StringBuilder();
-        
-        String returnMelody = melody; 
         for (int i = 0; i < playTime; i++) {
             sb.append(melody.charAt(i % melody.length()));
         }
-        
-        System.out.println("최종 재생된 멜로디: " + sb.toString());
-        return sb.toString(); // 문자열로 변화해서 return.
+        return sb.toString();
     }
     
-    public boolean check(String m, String playMelody, int playTime) {       
+    public boolean check(String m, String playMelody) {       
         return playMelody.contains(m);
     }
     
     public String solution(String m, String[] musicinfos) {
-        String answer = "";
+        m = convertSharp(m); // # 처리
+        String answer = "(None)";
+        int maxPlayTime = -1;
         
         for (String info : musicinfos) {
             String[] arr = info.split(",");
             
             int startTime = convertToMin(arr[0]);
             int endTime = convertToMin(arr[1]);
-            String title = arr[2];
-            String melody = arr[3];
-            String playMelody = buildMelody(melody, endTime - startTime);
+            int playTime = endTime - startTime;
             
-
-            if (check(m, playMelody, endTime - startTime)) {
-                answer += title;
+            String title = arr[2];
+            String melody = convertSharp(arr[3]); // # 처리
+            String playMelody = buildMelody(melody, playTime);
+            
+            if (check(m, playMelody)) {
+                if (playTime > maxPlayTime) {
+                    maxPlayTime = playTime;
+                    answer = title;
+                }
             }
         }
         
