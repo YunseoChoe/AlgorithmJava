550cdc486d7a2adec8e298158ee6950371c90cb6
최윤서 <128898996+YunseoChoe@users.noreply.github.com>
2026-02-04 21:31:55 +0900

    [Silver IV] Title: ATM, Time: 212 ms, Memory: 19960 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,58 +1,30 @@
-// ATM
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
-    private static int n;
-    private static int[] p;
-    private static int[] answer;
-    private static int totalMinValue = 0;
 
-    public static void getMinValue() {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                totalMinValue += p[answer[j] - 1];
-            }
-        }
-    }
-
-    public static void sort() {
-        // p 복사
-        int[] copyP = new int[n];
-        for (int i = 0; i < n; i++) {
-            copyP[i] = p[i];
-        }
-
-        // for문 돌때마다 제일 작은 값 찾고, 그 값은 max나 -1로 변경.
-        for (int i = 0; i < n; i++) {
-            // 최솟값 찾기
-            int minValue = 1001;
-            int minIndex = 0;
-            for (int j = 0; j < n; j++) {
-                
-                if (minValue > copyP[j]) {
-                    minValue = copyP[j];
-                    minIndex = j;
-                }
-            }
-
-            // 다 찾으면
-            answer[i] = minIndex + 1;
-            copyP[minIndex] = 1001;
-        }
-    }
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        p = new int[n];
-        answer = new int[n];
+
+        int n = sc.nextInt();
+        int[] p = new int[n];
+
         for (int i = 0; i < n; i++) {
             p[i] = sc.nextInt();
         }
 
-        sort();
-        getMinValue();
-        
+        // 오름차순 정렬
+        Arrays.sort(p);
+
+        // 누적합 계산
+        int totalMinValue = 0;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += p[i]; // 현재 사람까지 걸린 시간
+            totalMinValue += sum; // 전체 대기 시간에 더하기
+        }
+
         System.out.println(totalMinValue);
-    }  
+    }
 }
