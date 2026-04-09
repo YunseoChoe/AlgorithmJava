eef67d469e6f0fc72e7ee1e9d2b820f5cc471dea
최윤서 <128898996+YunseoChoe@users.noreply.github.com>
2026-04-10 00:14:02 +0900

    [Silver V] Title: 국회의원 선거, Time: 176 ms, Memory: 17756 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -1,68 +1,46 @@
-import java.util.*;
+// BOJ 1417 우선순위 큐 사용.
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Main {
-    static int n, cnt = 0;
+    static int n, ans = 0;
     static int[] candidate;
-
-    public static boolean isFirst() {
-        for (int i = 1; i < n; i++) {
-            if (candidate[i] >= candidate[0]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static int findMaxValueCandidateIdx() {
-        int maxValue = -1;
-        int maxValueIdx = -1;
-        for (int i = 1; i < n; i++) {
-            if (maxValue < candidate[i]) {
-                maxValue = candidate[i];
-                maxValueIdx = i;
-            }
-        }
-        
-        return maxValueIdx;
-    }
-
+    static PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // 최대힙
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // 최대힙
-
+        
         n = sc.nextInt();
+        if (n == 1) {
+            System.out.println(0);
+            return;
+        }
         
         candidate = new int[n];
 
         for (int i = 0; i < n; i++) {
             candidate[i] = sc.nextInt();
-            // pq.add(candidate[i]);
-        }
-
-        // 후보자가 1명뿐일 때
-        if (candidate.length < 2) {
-            System.out.println(0);
-            return;
-        }
-
-        // 다솜이가 1등이 될 때까지 반복
-        while (!isFirst()) {
-            // 한 후보씩 줄이기
-            // 가장 큰 후보 찾기
-            int maxIdx = findMaxValueCandidateIdx();
-           
-            // 투표 값 가져오기
-            candidate[0] += 1;
-            candidate[maxIdx] -= 1;
-            cnt++;
-
-            // 비교
-            if (isFirst()) {
-                break;
+            // 다솜이 제외 모든 참가자의 수 저장
+            if (i != 0) {
+                pq.add(candidate[i]);
             }
-           
-           
         }
-        System.out.println(cnt);
+
+        int dasomScore = candidate[0];
+
+        // 다솜이가 1등일 때 까지 반복
+        while (dasomScore <= pq.peek()) {
+            // pq에서 최대값 꺼내서 -1
+            int maxValue = pq.poll();
+            pq.add(maxValue -1);
+
+            // 다솜 +1
+            dasomScore++;
+
+            ans++;
+        }
+
+        System.out.println(ans);
     }
 }
