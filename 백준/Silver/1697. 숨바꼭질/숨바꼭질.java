df50161320bec193c36b00df2e162e8e2ff1a9ad
최윤서 <128898996+YunseoChoe@users.noreply.github.com>
2026-04-28 10:05:24 +0900

    [Silver I] Title: 숨바꼭질, Time: 216 ms, Memory: 26612 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,39 @@
+// 숨바꼭질
+import java.util.*;
+
+public class Main {
+    static int n, k;
+    static boolean[] visited = new boolean[100001];
+
+    public static void main (String[] main) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        solve(n);
+    }
+
+    public static void solve(int n) { // bfs
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {n, 0}); // 시간은 0초부터
+        visited[n] = true;
+
+        while (!queue.isEmpty()) {
+            int cur[] = queue.poll();
+
+            // 수빈 == 동생
+            if (cur[0] == k) {
+                System.out.println(cur[1]);
+                break;
+            }
+            
+            // 3가지의 위치 확인
+            int[] nextPositions = {cur[0] - 1, cur[0] + 1, cur[0] * 2};
+            for (int next : nextPositions) {
+                if (0 <= next && next <= 100000 && !visited[next]) {
+                    queue.add(new int[] {next, cur[1] + 1});
+                    visited[next] = true;
+                }
+            }
+        }
+    }
+}
