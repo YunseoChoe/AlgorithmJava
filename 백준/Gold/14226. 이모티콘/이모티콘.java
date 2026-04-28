be4e38f36134f4392493051afd07b81c8c1d4d8b
최윤서 <128898996+YunseoChoe@users.noreply.github.com>
2026-04-28 10:16:02 +0900

    [Gold IV] Title: 이모티콘, Time: 184 ms, Memory: 19452 KB -BaekjoonHub

---
+++ 백준/Gold/14226. 이모티콘/이모티콘.java
@@ -0,0 +1,52 @@
+// 이모티콘
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int s;
+    static boolean[][] visited = new boolean[1001][1001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        s = sc.nextInt();
+
+        solve(); 
+    }
+
+    public static void solve() { // bfs
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {1, 0, 0}); // 전체 이모티콘 개수, 시간, 복사 이모티콘 개수
+        visited[1][0] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int screen = cur[0]; 
+            int time = cur[1];
+            int clip = cur[2];
+
+            // 종료 조건
+            if (screen == s) {
+                System.out.println(time);
+                break;
+            }
+
+            // 1. 복사
+            if (!visited[screen][screen]) {
+                visited[screen][screen] = true;
+                queue.add(new int[]{screen, time + 1, screen});
+            }
+
+            // 2. 붙여넣기
+            if (clip > 0 && screen + clip <= 1000 && !visited[screen + clip][clip]) {
+                visited[screen + clip][clip] = true;
+                queue.add(new int[]{screen + clip, time + 1, clip});
+            }
+
+            // 3. 삭제
+            if (screen > 0 && !visited[screen - 1][clip]) {
+                visited[screen - 1][clip] = true;
+                queue.add(new int[]{screen - 1, time + 1, clip});
+            }
+        }
+    }
+}
