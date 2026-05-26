a4ff33b9375545d6713ccb50637dcaf0a43df1b6
최윤서 <128898996+YunseoChoe@users.noreply.github.com>
2026-05-26 20:24:57 +0900

    [level 2] Title: 전화번호 목록, Time: 199.10 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        int n = phone_book.length;
+        
+        Arrays.sort(phone_book); // 정렬
+
+        for (int i = 0; i < n - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
